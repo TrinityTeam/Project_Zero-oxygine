5a4d4b2d95919781994d15fb34c0946270a77037
Harrm <harrm@mail.ru>
2017-01-02 18:21:34 +0300

    All pathes and options now may be specified through this script command line options

--- proj.cmake/run.sh
+++ proj.cmake/run.sh
@@ -1,16 +1,46 @@
-#!/usr/bin/env sh
+#!/bin/bash
 
-mkdir build
-cd build
+until [ -z $1 ]
+do
+    printf '%s' ""
 
-#generate cmake project in the "build" folder
-cmake ..
+	case "$1" in
+	    '-c'|'--cmake-options')
+    		CMAKE_OPTIONS="$2"
+            shift
+    	;;
+    	'-m'|'--make-options')
+    		MAKE_OPTIONS="$2"
+            shift
+    	;;
+    	'-r'|'--oxygine-root')
+    		OXYGINE_ROOT=$(readlink -f "$2")
+            shift
+    	;;
+    	'-b'|'--build-dir')
+    		BUILD_DIR=$(readlink -f "$2")
+            shift
+        ;;
+    	'-d'|'--data-dir')
+    		DATA_DIR=$(readlink -f "$2")
+            shift
+    	;;
+    	*)
+    	    printf '%s\n' "Unknown option: $1"
+    	;;
+	esac
+	shift
+done
 
-#build it
-make
+BUILD_DIR=${BUILD_DIR:-"$(readlink -f build)"}
+DATA_DIR=${DATA_DIR:-"$(readlink -f ../data)"}
 
-#move to working data folder with resources
-cd ../../data
+CMAKE_OPTIONS="$CMAKE_OPTIONS -DOXYGINE_ROOT=${OXYGINE_ROOT:?Error: Oxygine has not been found; Please specify OXYGINE_ROOT(set environment variable or specify with -r|--oxygine-root argument) to the proper path to the Oxygine}"
 
-#run executable
-../proj.cmake/build/Project_Zero
+mkdir -p "$BUILD_DIR"
+cd "$BUILD_DIR"
+
+cmake .. $CMAKE_OPTIONS && make $MAKE_OPTIONS || exit 1
+
+cd "$DATA_DIR"
+"$BUILD_DIR"/Project_Zero
